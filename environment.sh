88dde1825ad57f63e2053451999bf453d6ca48eb
Samuel Huang <lspss95207@gmail.com>
2020-10-10 03:56:44 +0000

    add  network_config to environment.sh

--- environment.sh
+++ environment.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 source catkin_ws/devel/setup.bash
 source network_env.sh
+echo "Remember to setup network"
+echo "ROS_MASTER_URI="$ROS_MASTER_URI
+echo "ROS_IP="$ROS_IP
+echo ""
 sudo pigpiod
