625b6256a699259d28179661a29da60cf6e23ce4
Samuel Huang <lspss95207@gmail.com>
2020-10-11 10:02:54 +0800

    change image in gui from raw to object_detection

--- catkin_ws/src/control/src/control_gui_qt.py
+++ catkin_ws/src/control/src/control_gui_qt.py
@@ -81,7 +81,7 @@ class ControlGUI(QWidget):
         rospy.init_node('control_gui', anonymous=True)
 
 
-        self.image_topic_name = rospy.get_param("control_gui_py/image_topic","/mask_image/image_raw/compressed")
+        self.image_topic_name = rospy.get_param("control_gui_py/image_topic","/object_detection_image/image_raw/compressed")
 
         rospy.Subscriber("/joy", Joy, self.joy_callback)
         rospy.Subscriber("/"+self.glz_name+"/cannon/move", Twist, self.cannon_data_callback)
